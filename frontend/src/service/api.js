b3b001e441bdc816a19a87cf4771314202946b8c
madhanv22 <madhankumarv40@gmail.com>
2025-10-09 18:12:11 +0530

    initial push

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://150.242.201.153:5001/api/",
+  baseURL: "http://localhost:5000/",
   headers: {
     'Content-Type': 'application/json',
   },
